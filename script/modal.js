89adb9afe80cfb27d5a0ded56a722fb5b353bb06
sepaev <sepaev@gmail.com>
2021-03-20 03:26:21 +0200

    menu

--- script/modal.js
+++ script/modal.js
@@ -1,13 +1,23 @@
+
 (() => {
   const refs = {
+    // константы модалки
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    // константы менюшки
+    phoneMenuBtn: document.querySelector('[data-menu-button]'),
+    menu: document.querySelector('[data-menu-button]'),
+    menuContainer: document.querySelector('[data-menu-container]'),
   };
+
+  // товарищи наблюдатели
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  refs.phoneMenuBtn.addEventListener('click', togglePhoneMenu);
+ 
+  // ФУНКЦИИ
   function toggleModal() {
     refs.modal.classList.toggle('backdrop__is-hidden');
 
@@ -15,4 +25,9 @@
     const method = isModalHidden ? "enableBodyScroll" : "disableBodyScroll";
     bodyScrollLock[method](refs.modal);
   }
-})();
+
+  function togglePhoneMenu() {
+    refs.menu.classList.toggle('is-open');
+        refs.menuContainer.classList.toggle('is-hidden');
+  }
+})()
